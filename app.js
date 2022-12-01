1e1958432fc459f848bbf5892eb58038ec3d6953
CJ Evans <cje.software.engineer@gmail.com>
2022-12-01 16:36:47 -0600

    init commit 3file framework with classes outline

---
+++ app.js
@@ -0,0 +1,49 @@
+/*
+Wrapping your code in an anonymous function like this prevents variable collision if it were to be integrated with another program, it also helps with security
+*/
+window.addEventListener('load', function() {
+
+    /** @type {HTMLCanvasElement} */
+    //initial JavaScript setup for canvas animation and Game Development *****START*****
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+    //CANVAS_WIDTH = canvas.width = 800;
+    //CANVAS_HEIGHT = canvas.height = 720;
+    //initial JavaScript setup for canvas animation and Game Development *****END*****
+    //image test
+    //ctx.fillStyle = 'white';
+    //ctx.fillRect(50, 50, 100, 150);
+    //^should create a white rectangle at 50px x / 50px y, that is 100px wide / 150px height
+
+    class InputHandler {
+
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+
+    }
+
+    function displayStatusText() {
+
+    }
+
+    function animate() {
+
+    }
+
+
+});

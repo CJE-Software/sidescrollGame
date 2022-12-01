64d1f0f46d27cf96049955e2a08d531966efa444
CJ Evans <cje.software.engineer@gmail.com>
2022-12-01 17:04:24 -0600

    added eventhandler for input added keys array

--- app.js
+++ app.js
@@ -18,7 +18,28 @@ window.addEventListener('load', function() {
     //^should create a white rectangle at 50px x / 50px y, that is 100px wide / 150px height
 
     class InputHandler {
-
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((   e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight')
+                        && this.keys.indexOf(e.key) === -1) {
+                        this.keys.push(e.key);
+                }
+                console.log(e.key, this.keys);
+            });
+            window.addEventListener('keyup', e => {
+                if (    e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys);
+            });
+        }
     }
 
     class Player {
@@ -41,9 +62,44 @@ window.addEventListener('load', function() {
 
     }
 
+    const input = new InputHandler();
+
     function animate() {
 
     }
 
 
 });
+
+/*
+ES6 arrow functions do not bind their own 'this', but they inherit the one from their parent scope, this is called 'lexical scoping' refer to class of InputHandler()
+if (e.key === 'ArrowDown' && this.keys.indexOf(e.key) === -1)
+the above code asks if key is equal to arrowdown key and if key is not already in the array called 'keys'
+CODE
+    class InputHandler {
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((   e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight')
+                        && this.keys.indexOf(e.key) === -1) {
+                        this.keys.push(e.key);
+                }
+                console.log(e.key, this.keys);
+            });
+            window.addEventListener('keyup', e => {
+                if (    e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys);
+            });
+        }
+    }
+CODE
+^^^the above code checks to see if a key has been pressed if it has it is added to an array called keys, everytime a key is pressed it is registered to an array everytime you release the key it is popped off of the array using the indexOf method inside the splice method to remove it
+*/

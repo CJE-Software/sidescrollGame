cc328a2b21807b3d9009fbc4f27666789ebda631
CJ Evans <cje.software.engineer@gmail.com>
2022-12-02 09:27:48 -0600

    added background image and auto scrolling with if conditional

--- app.js
+++ app.js
@@ -92,7 +92,25 @@ window.addEventListener('load', function() {
     }
 
     class Background {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backgroundImage');
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 720;
+            this.speed = 20;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
 
+        }
+        update() {
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x = 0;
+        }
     }
 
     class Enemy {
@@ -109,10 +127,13 @@ window.addEventListener('load', function() {
 
     const input = new InputHandler(); //by instantiating the code here all the code in the class called InputHandler() will be run ^.^
     const player = new Player(canvas.width, canvas.height);
-    player.draw(ctx);
+    const background = new Background(canvas.width, canvas.height);
+    //player.draw(ctx);
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(ctx);
+        background.update();
         player.draw(ctx);
         player.update(input);
         requestAnimationFrame(animate);
